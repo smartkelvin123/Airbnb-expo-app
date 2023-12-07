016182ab5ddc4ee8ef780d2b36037236ac44d6ba
unknown <smartkelvin66@gmail.com>
2023-12-07 13:49:00 +0100

    smart

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,22 @@
 import { View, Text } from "react-native";
 import { Link, Stack } from "expo-router";
-import React from "react";
+import React, { useState } from "react";
 
 import ExploreHeader from "../components/ExploreHeader";
 import Listings from "../components/Listings";
 
 const Page = () => {
+  const [category, setCategory] = useState(" tiny home ");
+
+  const onDataChanged = (category: string) => {
+    console.log("onDataChanged", category);
+  };
+
   return (
     <View>
       <Stack.Screen
         options={{
-          header: () => (
-            <ExploreHeader
-            //  onCategoryChanged={function (category: string): void {
-            //   throw new Error("Function not implemented.");
-            // } }
-            />
-          ),
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
         }}
       />
       <Listings />
